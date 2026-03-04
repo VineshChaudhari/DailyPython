01894a25e5895dcec7eb02ce3ce78ca0c9321fcf
VineshChaudhari <chaudharivineshb09@gmail.com>
2026-03-04 23:28:28 +0530

    Calculator update

--- Heisenberg.py
+++ Heisenberg.py
@@ -1,9 +1,25 @@
-name = input("Enter your name: ")
-print("Hi",name,"How are you doing")
+#Basic Calculator#
 
-print("Here is Simple Additionin Python")
+print("Enter Number to do calculating operation")
+a = float(input("Number 1: "))
+b = float(input("Number 2: "))
 
-a = 5
-b = 6
+print("What operation you want to do Adition, Subtraction, Multiplication, Division")
+operation = input("Enter operation: ")
 
-print("If a is 5 and b is 6 a+b is: ",a+b)
+Addition = a + b
+Subtraction = a-b
+Multiplication = a*b
+Division = a/b
+
+if operation == "Addition":
+    print("Adition is a+b: ",Addition)
+
+elif operation == "Subtraction":
+    print("Subtraction is a-b: ", Subtraction)
+
+elif operation == "Multiplication":
+    print("Multiplication is a*b: ", Multiplication)
+
+else:
+    print("Division is a/b: ", Division)
