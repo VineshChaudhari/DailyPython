b90b4fbb32ab5c93acc43d6e7f29c3a1a728d5f4
VineshChaudhari <chaudharivineshb09@gmail.com>
2026-03-07 02:52:30 +0530

    Indentation,if-else statement,operators

--- Heisenberg.py
+++ Heisenberg.py
@@ -1,2 +1,12 @@
-age = int(input("Enter your age: "))
-print("You are",age,"years old")
+a = float(input("Enter any number: "))
+b = float(input("Enter any number: "))
+
+if a > b:
+    print(a,"is greater in value than",b)
+    print("Still inside if block")
+
+else:
+    print(b,"is greater in value than",a)
+    print("Inside if block")
+    
+print("Outside if block")
