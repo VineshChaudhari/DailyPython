4792fccc6987c968e6f4516f6af5ff7d30f09900
VineshChaudhari <chaudharivineshb09@gmail.com>
2026-03-07 12:08:43 +0530

    Lists

--- Heisenberg.py
+++ Heisenberg.py
@@ -1,2 +1,15 @@
-a = "Vinesh"
+a = [x for x in range(5)]
+print (a)
+
+a = []
+for i in range(5):
+    a.append(i)
+print(a)
+
+a = [1,2,3]
+a.append(4)
+print(a)
+
+a = [1,2,4]
+a.insert(3,2)
 print(a)
