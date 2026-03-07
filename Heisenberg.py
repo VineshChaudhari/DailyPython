388b2eb61f6748be2fc65223413d38f889311554
VineshChaudhari <chaudharivineshb09@gmail.com>
2026-03-07 11:43:50 +0530

    Lists

--- Heisenberg.py
+++ Heisenberg.py
@@ -1,12 +1,2 @@
-a = float(input("Enter any number: "))
-b = float(input("Enter any number: "))
-
-if a > b:
-    print(a,"is greater in value than",b)
-    print("Still inside if block")
-
-else:
-    print(b,"is greater in value than",a)
-    print("Inside if block")
-    
-print("Outside if block")
+a = "Vinesh"
+print(a)
