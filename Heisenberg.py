2e291c9c494214c9632f9fa79f1719a5c344eefb
VineshChaudhari <chaudharivineshb09@gmail.com>
2026-03-09 23:04:44 +0530

    Integration using Python

--- Heisenberg.py
+++ Heisenberg.py
@@ -1,18 +1,6 @@
-#List Comprehension
+x = int(input("Enter Base Number: "))
+n = int(input("Enter the power of x: "))
 
-#Using for loop
-squares = [x*x for x in range(1,6)]
-print (squares)
+equation = x^n+1/n+1
 
-#Using single if condition
-evens = [x for x in range (1,11) if x % 2 == 0]
-print (evens)
-
-#Using if-else condition
-result = ["Even" if x % 2 == 0 else "Odd" for x in range(1,6)]
-print (result)
-
-# Using Multiple if conditions
-a = [x for x in range (1,31) if x % 2 == 0 if x % 3 == 0]
-print(a)
- 
+print("Integration is",equation)
